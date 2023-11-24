ea98a40596abb98468f89e7811ade11d4c7d01b3
landwarrior <tech_land_equilibrium@outlook.jp>
2023-11-25 00:26:19 +0900

    AIにリファクタリングしてもらった

--- decos.py
+++ decos.py
@@ -1,13 +1,14 @@
+import functools
+
 def log(logger):
     def log_wrapper(func):
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             logger.info(f"--START-- {func.__name__}")
             res = func(*args, **kwargs)
             logger.info(f"res: {res}")
             logger.info(f"-- END -- {func.__name__}")
             return res
-
-        wrapper.__doc__ = func.__doc__
 
         return wrapper
 
